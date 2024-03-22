5649e72ca00576f35ad126c23b9a6d3464c0690d
lukaszmosionek <mosioneklukasz@gmail.com>
2024-03-22 18:33:28 +0100

    Initial commit

---
+++ resources/lang/pl/home.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+    
+    'metaDescription' => 'Pobieraj bez limitu muzykę z youtube.com. Zupełnie za darmo konwertuj yt na pliki mp3.',
+    'metaTitle' => 'Pobieraj muzykę z youtube! Darmowy konwerter z yt na mp3.',
+    'metaKeys' => 'youtube mp3, converter yt to mp3, YouTube to mp3, download youtube mp3, free download youtube mp3',
+    
+    'title' => 'Konverter YouTube na MP3',
+    'titleContent' => 'Youtube na mp3',
+    'content' => 'Wklej link z youtube i kliknij "pobierz mp3". Poczekaj chwile aż utwór się przekonwertuje i wybierz odpowiednią jakość.',
+    'buttonDownload' => 'POBIERZ JAKO MP3',
+    'lastDownload' => 'Ostatnio pobierane:',
+    'backToTop' => 'Idź na górę',
+    'footer' => 'Darmowy konwerter YouTube na plik mp3',
+    
+];
